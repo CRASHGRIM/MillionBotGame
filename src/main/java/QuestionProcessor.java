dfc666192dd0582cabf1b3faecb2f5b56ea5e6e6
Dims7 <steelbreedman@gmail.com>
2018-11-28 18:05:53 +0300

    wip

---
+++ src/main/java/QuestionProcessor.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class QuestionProcessor {
+    ArrayList<Question> easyQuestions;
+    ArrayList<Question> mediumQuestions;
+    ArrayList<Question> hardQuestions;
+
+}
